d4bd0295de1ba9349b2b35eca95a192722753130
github kite0311 <19970311kk@gmail.com>
2021-12-19 22:42:22 +0900

    文字間の空白の変更

--- Main/contact/confirm.php
+++ Main/contact/confirm.php
@@ -39,32 +39,32 @@ EOM;
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="Content-Language" content="ja" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>製造所固有記号</title>
-    <link rel="stylesheet" href="../stylesheet/stylesheet.css">
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-    </head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="ja" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>製造所固有記号</title>
+<link rel="stylesheet" href="../stylesheet/stylesheet.css">
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+</head>
 <body>
-   <h1 align="center">製造所固有記号検索</h1>
-   <form action="confirm.php" method="post">
-      <input type="hidden" name="name" value="<?php echo htmlspecialchars($name,ENT_QUOTES,"UTF-8"); ?>">
-      <input type="hidden" name="email" value="<?php echo htmlspecialchars($email,ENT_QUOTES,"UTF-8"); ?>">
-      <input type="hidden" name="category" value="<?php echo htmlspecialchars($category,ENT_QUOTES,"UTF-8"); ?>">
-      <input type="hidden" name="content" value="<?php echo htmlspecialchars($content,ENT_QUOTES,"UTF-8"); ?>">
-      <p>お問い合わせ内容は下記の通りでよろしいでしょうか？<br>よろしければ「送信する」ボタンをクリックして下さい</p>
-      <dl>
-         <dt>お名前</dt>
-         <dd><?php echo htmlspecialchars($name,ENT_QUOTES,"UTF-8");?></dd>
-         <dt>メールアドレス</dt>
-         <dd><?php echo htmlspecialchars($email,ENT_QUOTES,"UTF-8");?></dd>
-         <dt><?php echo htmlspecialchars($category,ENT_QUOTES,"UTF-8");?></dt>
-         <dt>お問い合わせ内容</dt>
-         <dd><?php echo htmlspecialchars($content,ENT_QUOTES,"UTF-8");?></dd>
-      </dl>
-      <input type="button" value="戻る" onClick="history.back()">
-      <button type="submit" name="submit">送信</button>
-   </form>
+<h1 align="center">製造所固有記号検索</h1>
+<form action="confirm.php" method="post">
+<input type="hidden" name="name" value="<?php echo htmlspecialchars($name,ENT_QUOTES,"UTF-8"); ?>">
+<input type="hidden" name="email" value="<?php echo htmlspecialchars($email,ENT_QUOTES,"UTF-8"); ?>">
+<input type="hidden" name="category" value="<?php echo htmlspecialchars($category,ENT_QUOTES,"UTF-8"); ?>">
+<input type="hidden" name="content" value="<?php echo htmlspecialchars($content,ENT_QUOTES,"UTF-8"); ?>">
+<p>お問い合わせ内容は下記の通りでよろしいでしょうか？<br>よろしければ「送信する」ボタンをクリックして下さい</p>
+<dl>
+   <dt>お名前</dt>
+   <dd><?php echo htmlspecialchars($name,ENT_QUOTES,"UTF-8");?></dd>
+   <dt>メールアドレス</dt>
+   <dd><?php echo htmlspecialchars($email,ENT_QUOTES,"UTF-8");?></dd>
+   <dt><?php echo htmlspecialchars($category,ENT_QUOTES,"UTF-8");?></dt>
+   <dt>お問い合わせ内容</dt>
+   <dd><?php echo htmlspecialchars($content,ENT_QUOTES,"UTF-8");?></dd>
+</dl>
+<input type="button" value="戻る" onClick="history.back()">
+<button type="submit" name="submit">送信</button>
+</form>
 </body>
 </html>
