82f28c3dcdccbb99bb49be3c1fa1411069cfeb9a
github kite0311 <19970311kk@gmail.com>
2021-12-21 00:10:32 +0900

    db接続時の要素を

--- Main/Join_sys/db_connect.php
+++ Main/Join_sys/db_connect.php
@@ -1,7 +1,7 @@
 <?php
-$dsn = "mysql:dbname=製造所固有記号;host=localhost;charset=utf8mb4";
-$username = "root";
-$password = "";
+define('DSN',"mysql:dbname=製造所固有記号;host=localhost;charset=utf8mb4");
+define('DB_USER', "root");
+define('DB_PASS', "");
 try {
     $db = new PDO($dsn, $username, $password);
 }catch (PDOException $e) {
