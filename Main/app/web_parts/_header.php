bd005ce75b8c7168a5a2f28a07c8648251d5fdd8
github kite0311 <19970311kk@gmail.com>
2021-12-30 23:25:41 +0900

    分割ファイル作成

---
+++ Main/app/web_parts/_header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Language" content="ja" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>製造所固有記号</title>
+        <link rel="stylesheet" href="../stylesheet/stylesheet.css">
+        <link rel="stylesheet" href="../stylesheet/navigation_menu.css">
+        <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    </head>
+    <body>
+        <h1 align="center">製造所固有記号検索</h1>
+        
---
+++ Main/app/web_parts/search_area.php
@@ -0,0 +1,18 @@
+<h2>検索フォーム</h2>
+    <form action="Symbol_view.php" method="get">
+    <!--Searh Contents Area-->
+        <div class="search_contents">
+            <table style="border-color: #b4d46e;">
+                <tr>
+                    <td class="label">記号:<span style="color: #FF0000">必須<span></td>
+                    <td class="search_symbol"><input type="text" name="symbol" size="5" max_length="10" placeholder="例: A" required></td>            
+                </tr>
+                <tr>
+                    <td class="label">製造者:</td>
+                    <td class="search_maker"><input type="text" name="maker_name" size="30" maxlength="50" class="maker" placeholder="例: ○○株式会社"></td>
+                </tr>
+                <tr>
+                    <td class="search_button"><input type="submit" value="検索" name="search" id="search"></td>
+                </tr>
+            </table>
+        </div>
