fa6e065b443981d6bbfd1e118690a0226358321c
github kite0311 <19970311kk@gmail.com>
2021-12-30 23:56:54 +0900

    パスの指定先を変更

--- Main/app/web_parts/_header.php
+++ Main/app/web_parts/_header.php
@@ -5,10 +5,9 @@
         <meta http-equiv="Content-Language" content="ja" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>製造所固有記号</title>
-        <link rel="stylesheet" href="../stylesheet/stylesheet.css">
-        <link rel="stylesheet" href="../stylesheet/navigation_menu.css">
+        <link rel="stylesheet" href="../../stylesheet/stylesheet.css">
+        <link rel="stylesheet" href="../../stylesheet/navigation_menu.css">
         <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     </head>
     <body>
         <h1 align="center">製造所固有記号検索</h1>
-        
