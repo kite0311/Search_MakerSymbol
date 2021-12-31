90a100e70ec02d648c3a9b3be74e6949f0dc0bc9
github kite0311 <19970311kk@gmail.com>
2021-12-31 22:32:56 +0900

    クラス処理を追加してコードの簡略化

--- Main/DataBase/dbsearch_symbol.php
+++ Main/DataBase/dbsearch_symbol.php
@@ -1,39 +1,34 @@
 <?php
+
+require_once("db_config.php");
 $sy_errors = array();
 $rows = array();
-if(isset($_GET["symbol"]) && isset($_GET["maker_name"])) { //DataCheck
-    $symbol = $_GET["symbol"];
-    $maker = $_GET["maker_name"];
-    if(!empty($symbol && $maker)) {
-        require_once("db_connect.php");
-                try {
-                    $dbh = new PDO($dsn,$username,$password);
-                    $stmt = $dbh->prepare("SELECT * FROM search WHERE symbol = (:symbol) AND maker_name LIKE (:maker_name)");
-                        if($stmt) {
-                            $stmt->bindValue(':symbol', $symbol, PDO::PARAM_STR);
-                            $stmt->bindValue(':maker_name', "%".$maker ."%", PDO::PARAM_STR);
-                            $stmt->execute();
-                            $rows = $stmt->fetchAll();
-                            $dbh = null;
-                        }
-                    } catch(PDOException $e) {
-                        echo 'DB接続エラー :' . $e->getMessage();
-                    }
-        }else if(!empty($symbol)){
-            require_once("db_connect.php");
-                try {
-                    $dbh = new PDO($dsn,$username,$password);
-                    $stmt = $dbh->prepare("SELECT * FROM search WHERE symbol = (:symbol) ");
-                        if($stmt) {
-                            $stmt->bindValue(':symbol', $symbol, PDO::PARAM_STR);
-                            $stmt->execute();
 
-                            $rows = $stmt->fetchAll();
-                            $dbh = null;
-                        }
-                    } catch(PDOException $e) {
-                        echo 'DB接続エラー :' . $e->getMessage();
-                    }
-                }
+/* フォームからの値を取得 */
+$symbol = filter_input(INPUT_GET,'symbol');
+$maker  = filter_input(INPUT_GET,'maker');
+
+/* $symbol,$makerに値が入っていた時 */
+if(!empty($symbol && $maker)) {
+    $dbh = new Connect();
+    $dbh->pdo();
+    $stmt = $dbh->pdo()->prepare("SELECT * FROM search WHERE symbol = (:symbol) AND maker_name LIKE (:maker_name)");
+    if($stmt) {
+        $stmt->bindValue(':symbol', $symbol, PDO::PARAM_STR);
+        $stmt->bindValue(':maker_name', "%".$maker ."%", PDO::PARAM_STR);
+        $stmt->execute();
+        $rows = $stmt->fetchAll();
     }
+/* $symbolのみ値が入力されていた時 */
+}else if(!empty($symbol)){
+    $dbh = new Connect();
+    $dbh->pdo();
+    $stmt = $dbh->pdo()->prepare("SELECT * FROM search WHERE symbol = (:symbol) ");
+    if($stmt) {
+        $stmt->bindValue(':symbol', $symbol, PDO::PARAM_STR);
+        $stmt->execute();
+        $rows = $stmt->fetchAll();
+    }
+}
+
 ?>
