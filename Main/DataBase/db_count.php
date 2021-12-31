502a8b935ec0552879568935ee9767241908498b
github kite0311 <19970311kk@gmail.com>
2021-12-31 17:26:35 +0900

    クラス呼び出し処理の簡略化

--- Main/DataBase/db_count.php
+++ Main/DataBase/db_count.php
@@ -1,16 +1,10 @@
 <?php
-require_once("db_connect.php");
-try {
-    $dbh = new PDO($dsn,$username,$password,);
-    $sql = "SELECT COUNT(*) FROM search";
-    $stmt = $dbh->query($sql);
 
-    foreach ($stmt as $row) {
-        echo "現在" .$row[0] ."件のデータが登録されています";
-    }
-    $dbh = null;
-    //DB接続エラー
-}catch(PDOException $e) {
-    echo 'DB接続エラー :' . $e->getMessage();
-}
+    require("db_connect.php");
+
+    $sql = "SELECT COUNT(*) FROM search";
+    $count = new Count();
+    $count->sql = $sql;
+    $count->DataCount($sql);
+
 ?>
