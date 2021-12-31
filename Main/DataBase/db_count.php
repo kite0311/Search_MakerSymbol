27273a7208d6f9c536c97012512e8df51f6bb66f
github kite0311 <19970311kk@gmail.com>
2021-12-31 17:41:50 +0900

    コメント追加

--- Main/DataBase/db_count.php
+++ Main/DataBase/db_count.php
@@ -1,10 +1,15 @@
 <?php
 
-    require("db_connect.php");
+    require("db_functions.php");
 
+    /* SQL文を定義 */
     $sql = "SELECT COUNT(*) FROM search";
-    $count = new DBCount();
+
+    /*インスタンスの作成 */
+    $count = new dbCount();
+    /* SQL文を代入 */
     $count->sql = $sql;
+    /* 関数の呼び出し */
     $count->DataCount($sql);
 
 ?>
