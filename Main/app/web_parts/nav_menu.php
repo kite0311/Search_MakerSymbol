3e1ecc8b3472a0e99d3a13bbb5f931f25819300f
github kite0311 <19970311kk@gmail.com>
2021-12-31 10:22:22 +0900

    ファイルの削除

--- Main/app/web_parts/nav_menu.php
+++
@@ -1,7 +0,0 @@
-<nav class="menu" id="menu">
-    <ul style="display: flex;">
-        <li><a href="../Home/What.html" class="What" aligen="center">固有記号とは？</a></li>
-        <li><a href="../Join_sys/signup.php">データベース管理</a></li>
-        <li><a href="../contact/contactForm.html" id="contact_form">問い合わせ</a></li>
-    </ul>
-</nav>
