a2b1b1269de00de554ba4a1b5d518c894ae5d0db
github kite0311 <19970311kk@gmail.com>
2021-12-31 09:50:46 +0900

    pathの指定先を変更

--- Main/app/web_parts/_header.php
+++ Main/app/web_parts/_header.php
@@ -5,8 +5,8 @@
         <meta http-equiv="Content-Language" content="ja" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>製造所固有記号</title>
-        <link rel="stylesheet" href="../../stylesheet/stylesheet.css">
-        <link rel="stylesheet" href="../../stylesheet/navigation_menu.css">
+        <link rel="stylesheet" href="../app/css/navigation_menu.css">
+        <link rel="stylesheet" href="../app/css/stylesheet.css">
         <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     </head>
     <body>
