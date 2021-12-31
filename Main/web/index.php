3933d935f336599f59df5b0669da399edff5258a
github kite0311 <19970311kk@gmail.com>
2021-12-31 10:22:59 +0900

    コメントの挿入

--- Main/web/index.php
+++ Main/web/index.php
@@ -1,8 +1,7 @@
 <?php
 
+//htmlタグの読み込み
 include("../app/web_parts/_header.php");
-include("../app/web_parts/nav_menu.php");
-
 //DB登録件数の表示
 require("../DataBase/db_count.php");
 //DB記号検索エリアの読み込み
@@ -12,6 +11,7 @@ include("../app/web_parts/search_area.php");
 
 <?php
 
+//フッターの読み込み
 include("../app/web_parts/_footer.php");
 
 ?>
