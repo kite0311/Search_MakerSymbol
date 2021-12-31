6dde14226a4969428e2e884ea94cb8f67331f736
github kite0311 <19970311kk@gmail.com>
2021-12-31 10:22:34 +0900

    navタグの記載追加

--- Main/app/web_parts/_header.php
+++ Main/app/web_parts/_header.php
@@ -11,3 +11,12 @@
     </head>
     <body>
         <h1 align="center">製造所固有記号検索</h1>
+        <header>
+            <nav class="menu" id="menu">
+                <ul style="display: flex;">
+                    <li><a href="../Home/What.html" class="What" aligen="center">固有記号とは？</a></li>
+                    <li><a href="../Join_sys/signup.php">データベース管理</a></li>
+                    <li><a href="../contact/contactForm.html" id="contact_form">問い合わせ</a></li>
+                </ul>
+            </nav>
+        </header>
