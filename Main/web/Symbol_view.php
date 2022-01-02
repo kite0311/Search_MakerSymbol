9d8d780e968b332447e4eb97155aa14ff25833cb
github kite0311 <19970311kk@gmail.com>
2022-01-03 00:16:38 +0900

    フォーム送信のバリデーション読み込みの追加

--- Main/web/Symbol_view.php
+++ Main/web/Symbol_view.php
@@ -1,23 +1,21 @@
 <?php
 
+//バリデーション処理の呼び出し
+require("../app/validate/validate_function.php");
+createToken();
+
 //フォームからのデータを処理
-require("../DataBase/dbsearch_symbol.php");
-
-?>
-
-<?php
+require("../app/DataBase/dbsearch_symbol.php");
 
 //htmlタグの読み込み
 include("../app/web_parts/_header.php");
+
 //DB記号検索エリアの読み込み
 include("../app/web_parts/search_area.php");
 
-?>
-
-<?php
-
 //検索結果表示
 require("../app/web_parts/search_result.php");
+
 //フッターの読み込み
 include("../app/web_parts/_footer.php");
 
--- Main/web/index.php
+++ Main/web/index.php
@@ -1,9 +1,15 @@
 <?php
 
+//バリデーション処理の呼び出し
+require("../app/validate/validate_function.php");
+createToken();
+
 //htmlタグの読み込み
 include("../app/web_parts/_header.php");
+
 //DB登録件数の表示
-require("../DataBase/db_count.php");
+require("../app/DataBase/db_count.php");
+
 //DB記号検索エリアの読み込み
 include("../app/web_parts/search_area.php");
 
